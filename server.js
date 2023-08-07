30ae9fe7d32ca51f0f6b349093a6ce0a93b7e447
Ricardo <ric.dalvarez@hotmail.com>
2023-08-07 22:58:06 +0200

    add message server running

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const mocks = require('ronin-mocks')
 const server = ronin.server()
 
 server.use('/', mocks.server(server.Router(), false, true))
-server.start()
+
+
+const _server = server.start()
+if (_server) { console.log("server started") }
